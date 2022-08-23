10bd9174c16a43215aaf1aa1ee1bb8bbd0f793b3
Wontory <tjddnjsjo@kakao.com>
2022-08-24 01:58:09 +0900

    prototype 1

--- src/KAT/Main.java
+++ src/KAT/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         // 수강 예정인 강의 수 입력
         int lectureNum = DataIO.setLectureNum();
-        Lecture[] lectureArchive = new Lecture[lectureNum];
+        Lecture[] lectures = new Lecture[lectureNum];
 
         //강의 및 분반 정보 입력
         for (int i = 0; i < lectureNum; i++) {
-            lectureArchive[i] = DataIO.setLectureData(i);
+            lectures[i] = DataIO.setLectureData(i);
         }
 
         // 테스트용 출력 코드
         /* System.out.println("====================\n        결과\n===================="); */
-        /* KAT.DataIO.printLectureInfo(lectureArchive); */
+        /* KAT.DataIO.printLectureInfo(lectures); */
 
         // 공강 요일 입력
         String freeDay = DataIO.setFreeDay();
@@ -24,11 +24,9 @@ public class Main {
         Combination combination = new Combination(lectureNum);
 
         // 시간표 조합
-        combination.lectureCombination(lectureArchive, 0, lectureNum, freeDay);
+        combination.lectureCombination(lectures, 0, lectureNum, freeDay);
 
-        // 시간표 조합 결과 출력
-        combination.printCombination(lectureArchive);
-
-        // 시간표 생성
+        // 시간표 조합 결과 출력 (Test)
+        combination.printCombination(lectures);
     }
 }
